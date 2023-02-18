d5cb26a24f299368d1f11a7287bdc65bef0ad26c
Benjamin Koomson <benkoomson07@gmail.com>
2023-02-17 16:24:22 -0800

    5-printf.c done

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/* 
+ * main - A program that prints output followed by a newline
+ * Return: 0
+ */
+int main (void)
+{
+printf("with proper grammar, but the outcome is a piece of art,");
+return (0);
+}
