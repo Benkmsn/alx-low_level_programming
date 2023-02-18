89f54f253102a69fab28fbe86585d4ac096cae43
Benjamin Koomson <benkoomson07@gmail.com>
2023-02-17 20:43:15 -0800

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -5,5 +5,5 @@
  * Return 1 (success)
  */
 int main(void)
-fput(and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+write(and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 return (1);
