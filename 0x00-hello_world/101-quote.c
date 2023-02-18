14d0c8d792adca73f284fa7e786ce1c44d96ec0a
Benjamin Koomson <benkoomson07@gmail.com>
2023-02-17 20:53:40 -0800

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
-#include <unistd.h>
+#include <stdlib.h>
 /**
- * main - Print a line to the standard error
- * Return 1 (success)
+ * main - Entry point
+ * Return: Always 1 (Success)
  */
 int main(void)
-write(and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+{
+write(STDOUT_FILENO, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 return (1);
+}
