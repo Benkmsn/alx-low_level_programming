805f5d5d81151d959f355699424138ae111fcc24
Benjamin Koomson <benkoomson07@gmail.com>
2023-02-28 00:46:13 -0800

    9-strcpy.c done

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -1,3 +1,4 @@
+#include "main.h"
 /**
  * _strcpy - a function that copies the string pointed to by src,
  * including the terminating null byte (\0),
@@ -15,3 +16,4 @@ char *_strcpy(char *dest, char *src)
 	while (*src)
 		*dest++ = *src++;
 	return (aux);
+}
